ad28a690a282d0e911572d421bf4c9a47a431f9c
guruprasad06 <mguruprasad2006@gmail.com>
2025-10-23 21:58:58 +0530

    Initial commit: add website files

---
+++ new.js
@@ -0,0 +1,7 @@
+let explore=document.querySelector("#explore-btn");
+let sub=document.querySelector("#submit");
+
+
+sub.addEventListener("click",()=>{
+    alert("THIS FORM IS SUBMITTED SUCCESSFULLY");
+});
